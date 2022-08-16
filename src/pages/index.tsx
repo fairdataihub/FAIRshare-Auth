6ea6f482997c32a49dc5a5d0dc1f7aa0edc3f3c3
Sanjay Soundarajan <sanjay.16@live.com>
2022-08-16 13:25:09 -0700

    refactor: ♻️ add index lottie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,13 @@ import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
 
+import LottieAnimation from '@/components/lotties';
+
+import heroLottie from '~/lotties/authentication.json';
+
 export default function Index() {
   return (
-    <div className="flex h-screen flex-col justify-between">
+    <div className="flex min-h-screen flex-col justify-between">
       <Head>
         <title>FAIRshare Authentication platform</title>
         <meta name="description" content="FAIRshare Authentication platform" />
@@ -12,11 +16,19 @@ export default function Index() {
       </Head>
 
       <main className="flex flex-1 flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold">
-          FAIRshare Authentication platform
+        <div className="mb-10 flex items-center justify-center">
+          <LottieAnimation
+            animationData={heroLottie}
+            width={200}
+            height={200}
+          />
+        </div>
+
+        <h1 className="text-5xl font-bold text-slate-800">
+          FAIRshare Authentication Platform
         </h1>
 
-        <div className="my-5 flex hidden space-x-2">
+        <div className="my-5 hidden space-x-2">
           <Link href="/github">
             <a className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
               GitHub OAuth
