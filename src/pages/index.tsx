e735383416308db3c218405c79d0652e4eae5083
Sanjay Soundarajan <sanjay.16@live.com>
2022-08-19 13:56:24 -0700

    feat: ✨ add error checks and add labels

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,11 @@ export default function Index() {
           FAIRshare Authentication Platform
         </h1>
 
-        <div className="my-5 hidden space-x-2">
+        <div
+          className={`my-5 space-x-2 ${
+            process.env.NODE_ENV === `development` ? 'flex' : 'hidden'
+          }`}
+        >
           <Link href="/github">
             <a className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
               GitHub OAuth
--- src/pages/zenodo/callback.tsx
+++ src/pages/zenodo/callback.tsx
@@ -31,10 +31,12 @@ const ZenodoOAuthCallback: React.FC<PageProps> = ({
     // remove session from storage - makes the session one time use only.
     sessionStorage.removeItem('zenodo-session');
 
-    // send the link to fairshare to authenticate the user
-    router.push(
-      `fairshare://auth-zenodo?session=${session_id}&token=${accessToken}&refreshToken=${refreshToken}`,
-    );
+    if (accessToken != `error` && refreshToken != `error`) {
+      // send the link to fairshare to authenticate the user
+      router.push(
+        `fairshare://auth-zenodo?session=${session_id}&token=${accessToken}&refreshToken=${refreshToken}`,
+      );
+    }
   });
 
   return (
@@ -51,50 +53,67 @@ const ZenodoOAuthCallback: React.FC<PageProps> = ({
             <h1 className="my-2 text-3xl font-medium">
               Successfully authenticated with Zenodo!
             </h1>
-            <p className="text-lg">
-              Copy and paste the following code into FAIRshare
-            </p>
-            <div className=" my-4 flex flex-row rounded-lg bg-slate-100 px-3 py-2">
-              <p>{accessToken}</p>
 
-              <div className="ml-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 rounded-md hover:cursor-pointer hover:bg-slate-300 active:translate-y-1 "
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  onClick={() => copyToClipboard(accessToken)}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-                  />
-                </svg>
+            <p className="text-center text-lg">
+              We will try to open your FAIRshare application and paste the
+              following details in automatically. <br /> Click &apos;Open&apos;
+              if you are asked to do so.
+            </p>
+
+            <div className="mt-12 mb-4 flex flex-row items-center space-x-4">
+              <span className="text-base font-medium"> Access Token: </span>
+
+              <div className="flex flex-row rounded-lg bg-slate-100 px-3 py-2">
+                <p>{accessToken}</p>
+                <div className="ml-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 rounded-md hover:cursor-pointer hover:bg-slate-300 active:translate-y-1 "
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    onClick={() => copyToClipboard(accessToken)}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-            <div className=" my-4 flex flex-row rounded-lg bg-slate-100 px-3 py-2">
-              <p>{refreshToken}</p>
-              <div className="ml-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 rounded-md hover:cursor-pointer hover:bg-slate-300 active:translate-y-1 "
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  onClick={() => copyToClipboard(refreshToken)}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-                  />
-                </svg>
+
+            <div className="mt-4 mb-12 flex flex-row items-center space-x-4">
+              <span className="text-base font-medium"> Refresh Token: </span>
+
+              <div className="flex flex-row rounded-lg bg-slate-100 px-3 py-2">
+                <p>{refreshToken}</p>
+                <div className="ml-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 rounded-md hover:cursor-pointer hover:bg-slate-300 active:translate-y-1 "
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                    onClick={() => copyToClipboard(refreshToken)}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
+
+            <p className="text-base text-slate-600">
+              You can copy and paste the following tokens manually if we are
+              unable to open your application.
+            </p>
           </>
         ) : (
           <div className="flex flex-row items-center justify-center">
