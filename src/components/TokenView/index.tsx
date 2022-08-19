4880393cee783af7578b25891a807288631218bf
Sanjay Soundarajan <sanjay.16@live.com>
2022-08-19 15:44:25 -0700

    fix: 🐛 move click function to container

--- src/components/TokenView/index.tsx
+++ src/components/TokenView/index.tsx
@@ -29,7 +29,10 @@ const TokenView: React.FC<TokenProps> = ({ label, token }) => {
 
         <div className="flex h-full flex-row items-center rounded-lg bg-slate-100 py-2 pl-3 pr-1">
           <p className="w-[300px] break-all">{token}</p>
-          <div className="ml-3 flex h-full items-center rounded-md hover:cursor-pointer hover:bg-slate-300">
+          <div
+            className="ml-3 flex h-full items-center rounded-md hover:cursor-pointer hover:bg-slate-300"
+            onClick={() => copyToClipboard(token)}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -37,7 +40,6 @@ const TokenView: React.FC<TokenProps> = ({ label, token }) => {
               viewBox="0 0 24 24"
               stroke="currentColor"
               strokeWidth={2}
-              onClick={() => copyToClipboard(token)}
             >
               <path
                 strokeLinecap="round"
