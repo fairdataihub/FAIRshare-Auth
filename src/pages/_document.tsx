fca52fbbd005a5a7d706a476fae7f5a0624042b7
Sanjay Soundarajan <sanjay.16@live.com>
2022-08-19 13:10:24 -0700

    chore: 🔧 add favicons

---
+++ src/pages/_document.tsx
@@ -0,0 +1,66 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            async
+            defer
+            data-website-id="0ec60d06-60fd-4204-a81b-548c2107254e"
+            src="https://umami.fairdataihub.org/mushroom.js"
+          />
+
+          <meta name="language" content="English" />
+          <meta name="author" content="Fair Data Innovations Hub" />
+
+          <meta property="og:type" content="website" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+
+          <link rel="icon" href="/favicon.ico" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
